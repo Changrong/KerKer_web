3b9f995292feef5eb6688aa546956b7875470b2a
Chang Rong Ko <fightclubex@hotmail.com>
2012-05-16 23:49:38 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'b1d348407d5b51d92ae89d069641ac4a8004e497dbc7cd62be80f5ec290ce9db2b42fcb723f86e954dbfa00aefc090ce4879d05baa566f8d36561bd2c3da213e'
